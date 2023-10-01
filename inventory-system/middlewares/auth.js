d8312033f1414fa77ef6a1c57f52cf491002d46e
Mansadhit301 <aadhit407@gmail.com>
2023-10-01 23:36:50 +0800

    Add Validasi Login

--- inventory-system/middlewares/auth.js
+++ inventory-system/middlewares/auth.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
+const prisma = require('../prisma/client');
 
 const verifyCallback = (req, resolve, reject) => async (err, user, info) => {
   if (err || info || !user) {
@@ -16,7 +17,18 @@ const auth = () => async (req, res, next) => {
   return new Promise((resolve, reject) => {
     passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject))(req, res, next);
   })
-    .then(() => next())
+    .then(async () => {
+      const token = req.headers.authorization.split(' ')[1];
+      const currentToken = await prisma.token.findFirst({
+        where: { token },
+      });
+
+      if (!currentToken.blacklisted) {
+        next();
+      } else {
+        throw new ApiError(httpStatus.FORBIDDEN, 'Please Login');
+      }
+    })
     .catch((err) => next(err));
 };
 
@@ -33,18 +45,5 @@ const adminAuth = () => async (req, res, next) => {
     })
     .catch((err) => next(err));
 };
-const loginAuth = () => async (req, res, next) => {
-  return new Promise((resolve, reject) => {
-    passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject))(req, res, next);
-  })
-    .then(() => {
-      if (req.user.blacklisted === false) {
-        next();
-      } else {
-        throw new ApiError(httpStatus.FORBIDDEN, 'Please Login');
-      }
-    })
-    .catch((err) => next(err));
-};
 
-module.exports = { auth, adminAuth, loginAuth };
+module.exports = { auth, adminAuth };
