e77e2c0aedba5648950045a4a96af769b553b5a6
ptrjs <peterjose64@gmail.com>
2023-12-08 13:45:07 +0700

    [dancy-todo-prisma] crud user

---
+++ fancy-todo-prisma/src/routes/user.route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    getUsers,
+    createUser,
+    updateUser,
+    deleteUser
+} = require("../controllers/user.controller.js");
+
+
+
+router.route("/")
+    .get(getUsers)
+    .post(createUser);
+
+router.route("/:id")
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
---
+++ fancy-todo-prisma/src/service/user.service.js
@@ -0,0 +1,132 @@
+const prisma = require("../../prisma/client.js");
+
+const getUsers = async () =>{
+    const res = await prisma.user.findMany();
+    return res;
+}
+
+const createUser = async ({name,email,phone})=>{
+    const res = await prisma.user.create({
+        data:{
+            name,
+            email,
+            phone
+        }
+    })
+    return res;
+}
+
+const updateUser = async({id, name, email, phone})=>{
+    const res = await prisma.user.update({
+        data:{
+            name,
+            email,
+            phone
+        },
+        where:{
+            id:parseInt(id)
+        }
+    })
+    return res;
+}
+    
+const deleteUser = async({id})=>{
+    const res = await prisma.user.delete({
+        where:{
+            id:parseInt(id)
+        }
+    })
+    return res;
+}
+
+module.exports = {
+    getUsers,
+    createUser,
+    updateUser,
+    deleteUser
+};
+
+
+// app.get("/user", async (req, res, next)=>{
+//     try {
+//         const result = await prisma.user.findMany()
+//         res.json({
+//             data:result,
+//             message:"User List"
+//         })    
+        
+//     } catch (error) {
+//         console.error(error);
+//         next(error);
+//     }
+// })
+
+// app.post("/user", async (req, res, next)=>{
+//     try {
+//         const {name, email, phone } = req.body 
+//         const result = await prisma.user.create({
+//             data:{
+//                 name:name,
+//                 email:email,
+//                 phone:phone
+//             }
+//         })
+//         res.json({
+//             data:result,
+//             message:"User Created"
+//         })
+        
+//     } catch (error) {
+//         console.error(error);
+//         next(error);
+//     }
+   
+// })
+
+// app.put("/user/:id", async (req, res, next)=>{
+//     try {
+//         const {id} = req.params
+//         const {name, email, phone} = req.body 
+//         const result = await prisma.user.update({
+//             data:{
+//                 name:name,
+//                 email:email,
+//                 phone:phone
+//             },
+//             where:{
+//                 id:parseInt(id)
+//             }
+//         })
+//         res.json({
+//             data:result,
+//             message:`User ${id} Updated`
+//         })
+        
+//     } catch (error) {
+//         console.error(error);
+//         next(error);
+//     }
+   
+// })
+
+
+// app.delete("/user/:id", async (req, res, next)=>{
+//     try {
+//         const {id} = req.params
+//         const result = await prisma.user.delete({
+//             where:{
+//                 id:parseInt(id)
+//             }
+//         })
+//         res.json({
+//             data:result,
+//             message:`User ${id} Deleted`
+//         })
+        
+//     } catch (error) {
+//         console.error(error);
+//         next(error);
+//     }
+   
+// })
+
