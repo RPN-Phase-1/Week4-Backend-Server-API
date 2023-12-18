45e51c776fa2e2781f3a428a0dd81dc60ef3be4b
ptrjs <peterjose64@gmail.com>
2023-12-18 20:33:56 +0700

    [inventory-system] winston

--- inventory-system/src/index.js
+++ inventory-system/src/index.js
@@ -1,20 +1,21 @@
-const app = require('./app.js');
-const prisma = require('../prisma/client.js');
+const app = require('./app');
+const config = require('./config/config');
+const logger = require('./config/logger');
+const prisma = require('../prisma/client');
 
 let server;
-let port = 3000
 
 if(prisma){
-  console.log('Connected to Database');
-  server = app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
+  logger.info('Connected to Database');
+  server = app.listen(config.port, () => {
+    logger.info(`Listening to port ${config.port}`);
   });
 }
 
 const exitHandler = () => {
   if (server) {
     server.close(() => {
-      console.log('Server closed');
+      logger.info('Server closed');
       process.exit(1);
     });
   } else {
@@ -23,7 +24,7 @@ const exitHandler = () => {
 };
 
 const unexpectedErrorHandler = (error) => {
-  console.log(error);
+  logger.error(error);
   exitHandler();
 };
 
@@ -31,7 +32,7 @@ process.on('uncaughtException', unexpectedErrorHandler);
 process.on('unhandledRejection', unexpectedErrorHandler);
 
 process.on('SIGTERM', () => {
-  console.log('SIGTERM received');
+  logger.info('SIGTERM received');
   if (server) {
     server.close();
   }
