575abdd922c89c99374ab8975944f4b18f70ec7f
ptrjs <peterjose64@gmail.com>
2023-12-18 20:48:30 +0700

    [inventory-system] morgan

--- inventory-system/src/app.js
+++ inventory-system/src/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 //const router = require('./routes');
+const config = require('./config/config')
+const morgan = require('./config/morgan');
 
 const app = express();
+
+
+if (config.env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
 
 // aktifin parsing json
 app.use(express.json());
