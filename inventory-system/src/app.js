42d07414030559c1a3da70dbd6d3124861ea9f38
ptrjs <peterjose64@gmail.com>
2023-12-19 19:15:59 +0700

    [inventory-system] BE security

--- inventory-system/src/app.js
+++ inventory-system/src/app.js
@@ -1,24 +1,40 @@
 const express = require('express');
-//const router = require('./routes');
-const config = require('./config/config')
+const httpStatus = require('http-status');
+// const router = require('./routes');
+const config = require('./config/config');
 const morgan = require('./config/morgan');
 const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
-const httpStatus = require('http-status');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const compression = require('compression');
+const cors = require('cors');
 
 const app = express();
-
 
 if (config.env !== 'test') {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
 
+// set security HTTP headers
+app.use(helmet());
+
 // aktifin parsing json
 app.use(express.json());
 
 // aktifin urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(xss());
+
+// gzip compression
+app.use(compression());
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
 
 app.get('/', (req, res) => {
   res.send('hello world');
