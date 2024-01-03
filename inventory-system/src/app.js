2befc4eb2eb828dc4dc2e1882d232a022cc60e68
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-01-03 22:44:46 +0700

    setting ap middleware rror.js

--- inventory-system/src/app.js
+++ inventory-system/src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const httpStatus = require('http-status');
+const router = require('./routes');
 const config = require('./config/config');
 const morgan = require('./config/morgan');
+const { errorConverter, errorHandler } = require('./middlewares/error');
+const ApiError = require('./utils/ApiError');
 
 const app = express();
 
@@ -9,11 +13,27 @@ if (config.env !== 'test') {
   app.use(morgan.errorHandler);
 }
 
+// aktifin parsing json
 app.use(express.json());
+
+// aktifin urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+  res.send('hello world');
 });
+
+// app.use(router)
+
+// send 404 error jika route tidak ada
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+// convert error jadi Instance API Error jika ada error yang tidak ketangkap
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
 
 module.exports = app;
