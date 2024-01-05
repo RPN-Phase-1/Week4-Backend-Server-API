6c72cbdf8adf1108493a8e7c151a918989176b23
ptrjs <peterjose64@gmail.com>
2024-01-05 15:22:24 +0700

    [inventory-system]  all crud done, auth done

--- inventory-system/src/routes/v1/product.route.js
+++ inventory-system/src/routes/v1/product.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const auth = require('../../middlewares/auth');
+const {auth} = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const productValidation = require('../../validations/product.validation');
 const productController = require('../../controllers/product.controller');
