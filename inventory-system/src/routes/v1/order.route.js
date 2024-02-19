4d3d65f5adef2f852def63d4b3394ee6aed578ae
mhaatha <mhafidza528@gmail.com>
2024-02-19 20:05:04 +0800

    Inventory-System [Authorization]

--- inventory-system/src/routes/v1/order.route.js
+++ inventory-system/src/routes/v1/order.route.js
@@ -3,18 +3,19 @@ const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const { orderValidation } = require('../../validations');
 const orderController = require('../../controllers/order.controller');
+const authorize = require('../../middlewares/authorize');
 
 const router = express.Router();
 
 router
   .route('/')
-  .post(auth(), validate(orderValidation.create), orderController.create)
-  .get(auth(), validate(orderValidation.getAll), orderController.getAll);
+  .post(auth(), authorize, validate(orderValidation.create), orderController.create)
+  .get(auth(), authorize, validate(orderValidation.getAll), orderController.getAll);
 
 router
   .route('/:orderId')
-  .get(auth(), validate(orderValidation.getId), orderController.getId)
-  .put(auth(), validate(orderValidation.update), orderController.update)
-  .delete(auth(), validate(orderValidation.deleted), orderController.deleted);
+  .get(auth(), authorize, validate(orderValidation.getId), orderController.getId)
+  .put(auth(), authorize, validate(orderValidation.update), orderController.update)
+  .delete(auth(), authorize, validate(orderValidation.deleted), orderController.deleted);
 
 module.exports = router;
