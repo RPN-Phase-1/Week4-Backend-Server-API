1998e56f6588acf52772b389625c1abd11a778e1
gerry <gerrydesrian48@gmail.com>
2024-03-04 15:00:40 +0700

    Inventory-System - [Error Handler]

--- inventory-system/app.js
+++ inventory-system/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const httpStatus = require('http-status');
 const config = require('./src/config/config');
 const morgan = require('./src/config/morgan');
+const { errorConverter, errorHandler } = require('./src/middlewares/error');
+const ApiError = require('./src/utils/apiError');
 // const router = require('./src/routes');
 const app = express();
 if (config.env !== 'test') {
@@ -14,5 +17,9 @@ app.get('/', (req, res) => {
 });
 
 // app.use(router);
-
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not Found'));
+});
+app.use(errorConverter);
+app.use(errorHandler);
 module.exports = app;
