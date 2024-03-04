6610ee1d86d0c39d00ac51d5a011cd028122c5fa
gerry <gerrydesrian48@gmail.com>
2024-03-04 11:18:16 +0700

    Inventory System - [Logging System]

--- inventory-system/app.js
+++ inventory-system/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
-
+const config = require('./src/config/config');
+const morgan = require('./src/config/morgan');
+// const router = require('./src/routes');
 const app = express();
-const router = require('./src/routes');
-
+if (config.env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('/', () => {
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.use(router);
+// app.use(router);
 
 module.exports = app;
