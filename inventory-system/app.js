6809e198c09080d1d1bac3f4e5dd76750ac4bb66
gerry <gerrydesrian48@gmail.com>
2024-03-05 22:14:54 +0700

    Inventory System - [Backend Security]

--- inventory-system/app.js
+++ inventory-system/app.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const httpStatus = require('http-status');
+const helmet = require('helmet');
+const compression = require('compression');
+const xss = require('xss-clean');
+const cors = require('cors');
 const config = require('./src/config/config');
 const morgan = require('./src/config/morgan');
 const { errorConverter, errorHandler } = require('./src/middlewares/error');
 const ApiError = require('./src/utils/apiError');
 // const router = require('./src/routes');
+
 const app = express();
 if (config.env !== 'test') {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
+app.use(helmet());
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+
+app.use(xss());
+
+app.use(compression);
+
+app.use(cors());
+app.options('*', cors());
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
