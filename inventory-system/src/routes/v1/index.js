16df7f75330bb5e1cf0ffc6febcd257618088f29
gerry <gerrydesrian48@gmail.com>
2024-03-09 11:39:49 +0700

    Inventory System - [Backend Validation]

--- inventory-system/src/routes/v1/index.js
+++ inventory-system/src/routes/v1/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRouter = require('./auth.route');
+const categoryRouter = require('./category.route');
 
 const router = express.Router();
 
@@ -7,6 +8,10 @@ const defaultRoutes = [
   {
     path: '/auth',
     route: authRouter,
+  },
+  {
+    path: '/category',
+    route: categoryRouter,
   },
 ];
 
