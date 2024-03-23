4880c5e6e668749800a0b5a24dac69f3ef7b492e
Refagi <renogivaro@gmail.com>
2024-03-23 14:38:43 +0700

    inventory system [set up]

---
+++ inventory-system/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = require('./routes');
+
+const app = express();
+
+// aktifin parsing json
+app.use(express.json());
+
+// aktifin urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+// app.use(router)
+
+module.exports = app;
