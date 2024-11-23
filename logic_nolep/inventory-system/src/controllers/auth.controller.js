565e333638ed9cfe80c9bd969eed74c354fa58fe
emangbolehsehansitu <136932079+emangbolehsehansitu@users.noreply.github.com>
2024-11-23 15:38:08 +0700

    Add files via upload

---
+++ logic_nolep/inventory-system/src/controllers/auth.controller.js
@@ -0,0 +1,31 @@
+const httpStatus = require('http-status');
+const catchAsync = require('../utils/catchAsync');
+const { authService, userService, tokenService } = require('../services');
+const ApiError = require('../utils/ApiError');
+
+const register = catchAsync(async (req, res) => {
+  const existingUser = await userService.getUserByEmail(req.body.email);
+
+  if (existingUser) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+  }
+
+  const userCreated = await userService.createUser(req.body);
+  const tokens = await tokenService.generateAuthTokens(userCreated);
+
+  console.log('User created:', userCreated); // Cek pengguna yang dibuat
+  
+  res.status(httpStatus.CREATED).send({ userCreated, tokens });
+});
+
+const login = catchAsync(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await authService.loginUserWithEmailAndPassword(email, password);
+  const tokens = await tokenService.generateAuthTokens(user);
+  res.send({ user, tokens });
+});
+
+module.exports = {
+  register,
+  login,
+};
