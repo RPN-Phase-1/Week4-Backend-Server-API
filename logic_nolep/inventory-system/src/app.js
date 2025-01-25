d7a4c3db22e16b8939dd587a556c51bda089d3ec
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-01-25 20:09:54 +0700

    feat: add api docs

--- logic_nolep/inventory-system/src/app.js
+++ logic_nolep/inventory-system/src/app.js
@@ -1,13 +1,26 @@
 const express = require("express");
-const router = require("./routes")
+const router = require("./routes");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+// Load Swagger YAML file
+const swaggerDocument = yaml.load('./api.yaml');
 
+// Swagger UI setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.options("*", cors());
+
+// API routes
 app.use('/api', router);
 
-app.use(router)
-
+// Export app module
 module.exports = app;
