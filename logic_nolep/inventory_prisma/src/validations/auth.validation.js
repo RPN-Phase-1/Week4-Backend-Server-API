c5f9400cd3427c0f054db08f6865145ebba1931a
Hannnzchool <laxus748799@gmail.com>
2025-02-20 04:26:13 +0700

    Feat(auth.validation): add login and register validation

---
+++ logic_nolep/inventory_prisma/src/validations/auth.validation.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+const { password } = require('./custom.validation');
+
+const register = {
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().custom(password),
+    name: Joi.string().required(),
+    role: Joi.string(), //fix error role tidak disertakan sehingga menjadi 400 dan badrequest karena role tidak diizinkan validation
+  }),
+};
+
+const login = {
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+};
+
+module.exports = {
+  register,
+  login,
+};
