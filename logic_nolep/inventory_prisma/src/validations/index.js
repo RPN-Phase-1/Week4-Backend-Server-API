c219767a1bdec784e5ad75750572460c7be5fbd5
Hannnzchool <laxus748799@gmail.com>
2025-02-20 04:31:06 +0700

    Chore(index): setup validation module index

---
+++ logic_nolep/inventory_prisma/src/validations/index.js
@@ -0,0 +1,6 @@
+module.exports.authValidation = require('./auth.validation');
+module.exports.categoryValidation = require('./category.validation');
+module.exports.productValidation = require('./product.validation');
+module.exports.orderValidaton = require('./order.validation');
+module.exports.orderItemValidation = require('./orderItem.validation');
+module.exports.userValidation = require('./user.validation');
