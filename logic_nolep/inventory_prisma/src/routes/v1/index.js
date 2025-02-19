9669da2f02164a667db4e9aa97f7fa3759904eb9
Hannnzchool <laxus748799@gmail.com>
2025-02-20 04:39:13 +0700

    Chore(index): setup route module index

---
+++ logic_nolep/inventory_prisma/src/routes/v1/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const authRoute = require('./auth.route');
+const categoryRoute = require('./category.route');
+const productRoute = require('./product.route');
+const userRoute = require('./user.route');
+const orderRoute = require('./order.route');
+const orderItemRoute = require('./orderItem.route');
+
+const router = express.Router();
+
+const defaultRoutes = [
+  {
+    path: '/auth',
+    route: authRoute,
+  },
+  {
+    path: '/category',
+    route: categoryRoute,
+  },
+  {
+    path: '/product',
+    route: productRoute,
+  },
+  {
+    path: '/users',
+    route: userRoute,
+  },
+  {
+    path: '/orders',
+    route: orderRoute,
+  },
+  {
+    path: '/order-items',
+    route: orderItemRoute,
+  },
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+module.exports = router;
